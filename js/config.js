f5da5bfb5785a545ca149f085561af83e98d770b
vishnavi06 <vishanavi6705@gmail.com>
2025-06-19 12:11:39 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_OmloBzsVz', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7d0kefqut5ous1uj3q8ulnrj43', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_AO70F36gw', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2vnchvbfh9ho1lki1ol068i2he', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
