8bcea75b3caffdc4292b465c9685c27a57d1950b
vishnavi06 <vishanavi6705@gmail.com>
2025-06-19 10:58:47 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_G4SCRGSCE', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7pkkv8lk65p5l1gubhnoa81u5o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
